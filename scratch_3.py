62743e3679c64fe7d87aa84a48c0ef5af4e822d6
Oscar Alvarez <mathoscaralvarez@gmail.com>
2018-04-12 14:18:39 -0500

    stuff

---
+++ scratch_3.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fileID = open('Mysong.txt','r')
+sns = fileID.readlines()
+snsVal = [int(x) for x in sns]
+
+print(sd)
+#
+# N = 500  # number of data points
+# t = np.linspace(0, 4*np.pi, N)
